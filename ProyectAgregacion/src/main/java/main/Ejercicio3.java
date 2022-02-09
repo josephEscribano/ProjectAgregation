8dc123383e650dcb5bf16ce8d227517b10a5901b
2dam <joseph.escribano1995@gmail.com>
2022-02-09 13:38:51 +0100

    commit ejercicio 4 terminado

---
+++ ProyectAgregacion/src/main/java/main/Ejercicio3.java
@@ -0,0 +1,59 @@
+package main;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import main.Constantes.Constantes;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+
+public class Ejercicio3 {
+
+
+//[{
+//        $project: {
+//            _id: 0,
+//                    title: 1,
+//                    month: {
+//                $month: {
+//                    $toDate: '$dtstart'
+//                }
+//            }
+//        }
+//    }, {
+//        $match: {
+//            $expr: {
+//                $eq: [
+//                '$month',
+//                        1
+//            ]
+//            }
+//        }
+//    }]
+
+    public static void main(String[] args) {
+        MongoClient mongo = MongoClients.create(Constantes.MONGODB);
+
+        MongoDatabase db = mongo.getDatabase(Constantes.DATABASE);
+        MongoCollection<Document> col = db.getCollection(Constantes.COLLECTION);
+
+        col.aggregate(Arrays.asList(new Document("$project",
+                        new Document("_id", 0L)
+                                .append("title", 1L)
+                                .append("month",
+                                        new Document("$month",
+                                                new Document("$toDate", "$dtstart")))),
+                new Document("$match",
+                        new Document("$expr",
+                                new Document("$eq", Arrays.asList("$month", 1L))))))
+                .into(new ArrayList<>())
+                .forEach(System.out::println);
+    }
+}
---
+++ ProyectAgregacion/src/main/java/main/Ejercicio4.java
@@ -0,0 +1,59 @@
+package main;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import main.Constantes.Constantes;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ejercicio4 {
+
+
+//    [{$match: {
+//        'event-location': {
+//            $regex: 'Latina'
+//        }
+//    }}, {$project: {
+//        _id: 0,
+//                month: {
+//            $month: {
+//                $toDate: '$dtstart'
+//            }
+//        }
+//    }}, {$match: {
+//        $expr: {
+//            $eq: [
+//            '$month',
+//                    1
+//  ]
+//        }
+//    }}, {$count: 'cantidad'}]
+    public static void main(String[] args) {
+
+        MongoClient mongo = MongoClients.create(Constantes.MONGODB);
+
+        MongoDatabase db = mongo.getDatabase(Constantes.DATABASE);
+        MongoCollection<Document> col = db.getCollection(Constantes.COLLECTION);
+
+        col.aggregate(Arrays.asList(new Document("$match",
+                        new Document("event-location",
+                                new Document("$regex", "Latina"))),
+                new Document("$project",
+                        new Document("_id", 0L)
+                                .append("month",
+                                        new Document("$month",
+                                                new Document("$toDate", "$dtstart")))),
+                new Document("$match",
+                        new Document("$expr",
+                                new Document("$eq", Arrays.asList("$month", 1L)))),
+                new Document("$count", "cantidad")))
+                .into(new ArrayList<>())
+                .forEach(System.out::println);
+
+
+    }
+}
