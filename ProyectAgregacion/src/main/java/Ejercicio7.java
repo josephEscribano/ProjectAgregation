775b447aeed40b72b7265561a95ffbfe31729fd9
2dam <joseph.escribano1995@gmail.com>
2022-02-10 11:52:27 +0100

    Falta ejercicio7 en java

---
+++ ProyectAgregacion/src/main/java/Ejercicio7.java
@@ -0,0 +1,14 @@
+public class Ejercicio7 {
+
+//    [{$group: {
+//        _id: '$event-location',
+//                totalEvent: {
+//            $sum: 1
+//        }
+//    }}, {$sort: {
+//        totalEvent: -1
+//    }}, {$limit: 1}]
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ ProyectAgregacion/src/main/java/main/Ejercicio5.java
@@ -0,0 +1,42 @@
+package main;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import main.Constantes.Constantes;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.mongodb.client.model.Accumulators.*;
+import static com.mongodb.client.model.Aggregates.*;
+import static java.util.Arrays.asList;
+import static com.mongodb.client.model.Sorts.*;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+public class Ejercicio5 {
+
+//    [{$group: {
+//        _id: '$event-location',
+//                totalEvent: {
+//            $sum: 1
+//        }
+//    }}]
+    public static void main(String[] args) {
+        MongoClient mongo = MongoClients.create(Constantes.MONGODB);
+
+        MongoDatabase db = mongo.getDatabase(Constantes.DATABASE);
+        MongoCollection<Document> col = db.getCollection(Constantes.COLLECTION);
+
+
+        col.aggregate(List.of(
+                group("$event-location", sum("totalEvents", 1))
+        )).into(new ArrayList<>()).forEach(System.out::println);
+
+    }
+}
